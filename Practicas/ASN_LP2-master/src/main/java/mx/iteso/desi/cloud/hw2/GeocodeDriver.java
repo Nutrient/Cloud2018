68779076d9b6d2a5ce0e458136cb517a2f6cc41f
Nutrient <carlobruno_fb@hotmail.com>
2018-10-20 00:43:59 -0500

    added hadoop base

--- Practicas/ASN_LP2-master/src/main/java/mx/iteso/desi/cloud/hw2/GeocodeDriver.java
+++ Practicas/ASN_LP2-master/src/main/java/mx/iteso/desi/cloud/hw2/GeocodeDriver.java
@@ -1,5 +1,13 @@
 package mx.iteso.desi.cloud.hw2;
 
+import mx.iteso.desi.cloud.GeocodeWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import mx.iteso.desi.cloud.Triple;
 
 public class GeocodeDriver {
@@ -12,6 +20,17 @@ public class GeocodeDriver {
     }
     
     /* TODO: Your driver code here */
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Localization");
+    job.setJarByClass(GeocodeDriver.class);
+    job.setMapperClass(GeocodeMapper.class);
+    job.setReducerClass(GeocodeReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(GeocodeWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
 	
   }
 }
--- Practicas/ASN_LP2-master/src/main/java/mx/iteso/desi/cloud/hw2/GeocodeMapper.java
+++ Practicas/ASN_LP2-master/src/main/java/mx/iteso/desi/cloud/hw2/GeocodeMapper.java
@@ -5,8 +5,19 @@ import mx.iteso.desi.cloud.GeocodeWritable;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.io.*;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class GeocodeMapper extends Mapper<LongWritable, Text, Text, GeocodeWritable> {
 
   /* TODO: Your mapper code here */
+  public void map(LongWritable key, Text value, Text value2, Context context) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while(itr.hasMoreTokens()) {
+          System.out.println(itr.nextToken());
+         // word.set(itr.nextToken());
+          //context.write(word, one);
+      }
+  }
   
 }
